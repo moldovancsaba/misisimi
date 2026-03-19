a4d4e0d344afc8f21ec15087eb085ce70b07f0d7
Moldovan Csaba Zoltan <moldovancsaba@gmail.com>
2026-03-19 12:36:57 +0100

    Initial commit: starting the project from zero

--- models/Message.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const MessageSchema = new mongoose.Schema({
-    text: { type: String, required: true },
-    ip: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Message', MessageSchema);
